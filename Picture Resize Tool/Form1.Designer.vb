e3d3006645b4637666066100e22788b769da14ad
amWRit <derasrocks@gmail.com>
2016-09-27 14:59:07 +0545

    splash

--- Picture Resize Tool/Form1.Designer.vb
+++ Picture Resize Tool/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class splash
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,106 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.labelStatus = New System.Windows.Forms.Label()
+        Me.labelCounter = New System.Windows.Forms.Label()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.counter = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(0, 0)
+        Me.ProgressBar1.MarqueeAnimationSpeed = 700
+        Me.ProgressBar1.Maximum = 105
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(100, 23)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'Timer1
+        '
+        '
+        'label1
+        '
+        Me.label1.Location = New System.Drawing.Point(0, 0)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(100, 23)
+        Me.label1.TabIndex = 2
+        '
+        'labelStatus
+        '
+        Me.labelStatus.BackColor = System.Drawing.Color.White
+        Me.labelStatus.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.labelStatus.Location = New System.Drawing.Point(0, 0)
+        Me.labelStatus.Name = "labelStatus"
+        Me.labelStatus.Size = New System.Drawing.Size(100, 23)
+        Me.labelStatus.TabIndex = 1
+        '
+        'labelCounter
+        '
+        Me.labelCounter.Location = New System.Drawing.Point(0, 0)
+        Me.labelCounter.Name = "labelCounter"
+        Me.labelCounter.Size = New System.Drawing.Size(100, 23)
+        Me.labelCounter.TabIndex = 0
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Location = New System.Drawing.Point(12, 192)
+        Me.ProgressBar2.MarqueeAnimationSpeed = 700
+        Me.ProgressBar2.Maximum = 105
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(260, 23)
+        Me.ProgressBar2.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(97, 152)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Loading..."
+        '
+        'counter
+        '
+        Me.counter.AutoSize = True
+        Me.counter.Location = New System.Drawing.Point(117, 197)
+        Me.counter.Name = "counter"
+        Me.counter.Size = New System.Drawing.Size(21, 13)
+        Me.counter.TabIndex = 6
+        Me.counter.Text = "0%"
+        '
+        'splash
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.BackgroundImage = Global.Picture_Resize_Tool.My.Resources.Resources.dafdafadf
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.counter)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.labelCounter)
+        Me.Controls.Add(Me.labelStatus)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "splash"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents label1 As Label
+    Friend WithEvents labelStatus As Label
+    Friend WithEvents labelCounter As Label
+    Friend WithEvents ProgressBar2 As ProgressBar
+    Friend WithEvents Label2 As Label
+    Friend WithEvents counter As Label
 End Class
--- Picture Resize Tool/Form1.vb
+++ Picture Resize Tool/Form1.vb
@@ -1,3 +1,22 @@
-﻿Public Class Form1
+﻿Imports System.Data
+Imports System.Data.OleDb
+Imports System.Configuration
+
+Public Class splash
+
+    Private Sub splash_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ProgressBar2.Value = ProgressBar2.Value + 5
+        counter.Text = CType(ProgressBar2.Value, String)
+        If ProgressBar2.Value = ProgressBar2.Maximum Then
+            Timer1.Enabled = False
+            Me.Hide()
+            homeForm.Show()
+        End If
+    End Sub
 
 End Class
